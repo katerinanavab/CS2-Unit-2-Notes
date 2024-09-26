beacdc1cc43a83fd19e65f9e8802eb5a0dd03d13
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-09-26 12:56:26 +0000

    added Scanner statements

--- Main.java
+++ Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner; // import the Scanner class
+
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Testing methods that RETURN values...");
 
+      // Construct a Scanner object
+      // note: System.in() is the computer's keyboard!
+      Scanner scan = new Scanner(System.in);
+
+      // Take input with the Scanner methods
+      System.out.println("Enter some text: ");
+      String inputStr = scan.nextLine();
+      System.out.println("Enter a whole number: ");
+      int inputInt = scan.nextInt();
+      System.out.println("Enter a decimal number: ");
+      double inputDouble = scan.nextDouble();
+
+      // Handle the user input (do something with the values)
+      // Use the value in a print statement
+      System.out.println("You entered: " + inputStr);
+      // Use values in an expression
+      int sum = inputInt + inputDouble;
       
    }
 }
